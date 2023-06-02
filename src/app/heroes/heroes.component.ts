9a68a010d8983714366a5a03adea5adcec187078
Larsen Friis <larsenfriis@icloud.com>
2023-06-02 11:40:44 +1000

    Initial commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { HEROES } from 'src/data/mock-heroes';
+import { Hero } from 'src/interfaces/hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
